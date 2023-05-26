5b46907733e50ab127ca7a130facba32fa025550
acatzk <joshuaimalay@gmail.com>
2023-05-26 21:14:50 +0800

    feat: create todos resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
 import { AppController } from './app.controller'
 import { UsersModule } from './users/users.module'
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TodosModule],
   controllers: [AppController],
   providers: [AppService]
 })
